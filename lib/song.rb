4f1e8d5d0361ffa1687fe35db599ea0c6cb607d5
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 13:32:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ new_song = Artist.new(filename)
 new_song.filename
 end 
 
-def artist_name=(name)
+def artist_name=(name_string)
    song_artist = Artist.find_or_create_by_name(name_string)
        @artist = song_artist
       @artist.add_song(self)
