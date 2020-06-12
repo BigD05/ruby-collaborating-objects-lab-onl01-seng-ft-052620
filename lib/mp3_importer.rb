6686b70c54ed975e737264de04bb5d6ef0e9ca01
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 13:34:54 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,6 +4,12 @@ attr_accessor :path
 def initialize(path)
   @path = path
 end 
+  def import
+    imported_files = self.files
+    imported_files.each do |file|
+      new_song = Song.new_by_filename(file)
+    end
+  end
 
  
 end 
