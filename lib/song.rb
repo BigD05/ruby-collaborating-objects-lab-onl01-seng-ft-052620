b70dd1df5ee68b05d99dac54c152bb2dc14e444e
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 13:01:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,9 @@ class Song
   end 
 def self.new_by_filename(filename)
 filename = Artist.new("Black or White")
-filename.artist
+filename.each do |artist| 
+  filename.artist
+end 
 end 
 
 def artist_name=(name)
