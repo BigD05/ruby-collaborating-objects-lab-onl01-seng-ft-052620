9bed8234776186623dc0ba59feeaec113c447cbb
kyle Dornan <kyledornan26@gmail.com>
2020-06-12 12:07:19 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song 
+  attr_accessor :name, :artist, :filename 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+   @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+def self.new_by_filename(filename)
+  new_instance = self.new_by_filename(filename)
+end 
+    
+end 
